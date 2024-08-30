8e523a1d8026709cb152454bf495fa5fe6b5ccfd
Mohammed Khaisar <129031568+khaisarsgithub@users.noreply.github.com>
2024-08-30 15:45:46 +0530

    Update settings.py

--- BetacraftCodeAnalyst/settings.py
+++ BetacraftCodeAnalyst/settings.py
@@ -28,8 +28,8 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-# ALLOWED_HOSTS = [ '*' ]
-ALLOWED_HOSTS = [".awsapprunner.com"]
+ALLOWED_HOSTS = [ '*' ]
+# ALLOWED_HOSTS = [".awsapprunner.com"]
 
 CSRF_COOKIE_SECURE = True
 CSRF_COOKIE_HTTPONLY = True
