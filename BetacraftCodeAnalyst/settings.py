0778ecaa41f2e748b2b1bc1b7caf6709e15c29f6
khaisarsgithub <mdkhaisars118@gmail.com>
2024-09-05 13:52:42 +0530

    updated requirements

--- BetacraftCodeAnalyst/settings.py
+++ BetacraftCodeAnalyst/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'django-insecure--@83p*)%2u@m50f&u=51evsy544=mirweoe3cghz!wgzk22o@y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
