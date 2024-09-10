dce30c344a79731e65eef8364afc344a1131e1d4
khaisarsgithub <mdkhaisars118@gmail.com>
2024-09-10 17:50:00 +0530

    updated wsgi

--- BetacraftCodeAnalyst/wsgi.py
+++ BetacraftCodeAnalyst/wsgi.py
@@ -14,13 +14,15 @@ from whitenoise import WhiteNoise
 import django
 
 
-django.setup()
 # Add your project directory to the Python path
 project_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(project_path)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BetacraftCodeAnalyst.settings')
 
+print(f"DJANGO_SETTINGS_MODULE: {os.environ.get('DJANGO_SETTINGS_MODULE')}")
+
+django.setup()
 
 application = get_wsgi_application()
 application = WhiteNoise(application)
